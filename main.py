683743dff3a10aaf4582873f47685fe75663ce87
Elmahdi Bahadia <elmahdibahadia@gmail.com>
2025-11-26 14:22:06 -0500

    Data Ingestion Pipeline

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
 from src.Wine_Quality_Prediction import logger
+from src.Wine_Quality_Prediction.pipeline.data_Ingestion_pipeline import DataIngestionTrainingPipeline
 
 logger.info("Welcome to our project Data Science Prediction to the Wine Quality")
+
+STAGE_NAME = "Data Ingestion Stage"
+
+try:
+    logger.info(f">>>>>>> stage {STAGE_NAME} started <<<<<<<<<")
+    data_ingestion = DataIngestionTrainingPipeline()
+    data_ingestion.initiate_data_ingestion()
+    logger.info(f">>>>>>> stage {STAGE_NAME} completed <<<<<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
